4adb71540060c8de4c04dd06ecd6ea9e373a8e27
Eric Lee <869909541@qq.com>
2018-07-13 15:09:17 +0800

    create origin project in Xcode

---
+++ DAT in Toutiao/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DAT in Toutiao
+//
+//  Created by Eric Handsome on 2018/7/13.
+//  Copyright © 2018年 Eric Handsome. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
